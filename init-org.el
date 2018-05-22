9a89f2dff7f7acd2a034a0cb36b5c0272c77b70d
Mathias Tausen <mta@gomspace.com>
2018-05-22 21:52:16 +0200

    Report definitions now in external .tji file

--- init-org.el
+++ init-org.el
@@ -93,45 +93,11 @@ leaves holiday \"Public holiday\" 2018-05-10 +2d,
        holiday \"Public holiday\" 2018-06-05 +1d
 ")
 
-;; customize taskjuggler reports
 (setq org-taskjuggler-default-reports
       (quote
-       ("textreport report \"Plan\" {
-  formats html
-  header '== %title =='
-
-  center -8<-
-    [#Plan Plan] | [#Resource_Allocation Resource Allocation]
-    ----
-    === Plan ===
-    <[report id=\"plan\"]>
-    ----
-    === Resource Allocation ===
-    <[report id=\"resourceGraph\"]>
-  ->8-
-}
-
-# A traditional Gantt chart with a project overview.
-taskreport plan \"\" {
-  headline \"Project Plan\"
-  columns bsi, name, start, end, effort, complete, effortleft, resources, chart { width 2500 }
-  loadunit shortauto
-  hideresource 1
-}
-
-# A graph showing resource allocation. It identifies whether each
-# resource is under- or over-allocated for.
-resourcereport resourceGraph \"\" {
-  headline \"Resource Allocation Graph\"
-  columns no, name, effort, weekly { width 1000 }
-  loadunit shortauto
-  hidetask ~(isleaf() & isleaf_())
-  sorttasks plan.start.up
-}
-
-include \"tasks-bookings.tji\"")))  ;; <- assuming tasks-bookings.tji exists!
-
-; daily { width 1000 }
+       ("
+include \"tasks-reports.tji\"
+include \"tasks-bookings.tji\"")))
 
 ;; add "include"
 (setq org-taskjuggler-valid-project-attributes
